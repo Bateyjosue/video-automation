74d5b79bc105ac76c0c0310bece3cf76bf4c18ab
Bateyjosue <josuebatey19@gmail.com>
2023-04-12 22:30:39 +0200

    Create relation between model

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: videos
+#
+#  id                      :bigint           not null, primary key
+#  youtube_id              :string           not null
+#  title                   :string           not null
+#  tags                    :string           default([]), is an Array
+#  chapter_markers         :text
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
+#  description_template_id :bigint           not null
+#
 class Video < ApplicationRecord
+  belongs_to :description_template
+  has_many :video_presenters
+  has_many :presentes, throght: :video_presenters
+  has_many :video_resources
 end
